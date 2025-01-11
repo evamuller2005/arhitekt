6e138ffc66515cc671dad4f5977147ad6cb5bdee
Gregor Vidrih <gv65015@student.uni-lj.si>
2025-01-11 13:53:27 +0100

    testno

--- Arhitekt/obj/Debug/net8.0/Arhitekt.AssemblyInfo.cs
+++ Arhitekt/obj/Debug/net8.0/Arhitekt.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Arhitekt")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7797a94f3a444f11413dbd96c7ecba589d7449ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+534a702582e4f6d3adaa6c1de8bbdda52b64fed7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Arhitekt")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Arhitekt")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
