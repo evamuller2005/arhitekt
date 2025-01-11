3a356a7fe2a4e4b2b983ac4a2da812c7fb9f80cc
Gregor Vidrih <gv65015@student.uni-lj.si>
2025-01-11 19:16:37 +0100

    sprememba connection stringa

--- Arhitekt/obj/Debug/net8.0/Arhitekt.AssemblyInfo.cs
+++ Arhitekt/obj/Debug/net8.0/Arhitekt.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Arhitekt")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a222dbcd4dc7ff11301efb9bdc1bb8633354218c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+186e1a888ba5dc1819366b372ba6331f7f1cde7f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Arhitekt")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Arhitekt")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
